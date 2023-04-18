8ffad23d781e3d7940d117bcf67bb20211c39e95
ykm989 <ykm989@naver.com>
2023-04-18 19:10:46 +0900

    단지 번호 붙이기(2667) 문제 완료

--- BackJoon.playground/Sources/2667.swift
+++ BackJoon.playground/Sources/2667.swift
@@ -1,38 +1,51 @@
+// 단지 번호 붙이기
+// 2023.04.18
+// https://www.acmicpc.net/problem/2667
+
 import Foundation
 
 public func solution(){
-    print("Hello")
-    let size = 7
-    let mapString = """
-                    
-                    0110100
-                    0110100
-                    0110101
-                    1110101
-                    0000111
-                    0100000
-                    0111110
-                    0111000
-                    """
-    
-    let map = mapString.components(separatedBy: "\n").map{
-        return $0.map{return $0.wholeNumberValue!}
+    let size = Int(readLine()!)!
+
+    var cnt = 0
+    var map = [[Int]]()
+
+    for _ in 0..<size {
+        map.append(Array(readLine()!).map{Int(String($0))!})
     }
-    
-    
-    func bfs(_ y: Int, _ x: Int, sum: Int) -> Int{
-        map[y][x] = 2 // visit
         
+    var queue : [Int] = []
+        
+    for i in 0..<size{
+        for j in 0..<size{
+            if map[i][j] == 1{
+                cnt = 1
+                queue.append(dfs(i, j))
+            }
+        }
+    }
+
+    print(queue.count)
+    for i in queue.sorted(by: <){
+        print(i)
+    }
+
+    func dfs(_ y: Int, _ x: Int) -> Int{
+        map[y][x] = 0
+
         let dx = [1,-1,0,0]
         let dy = [0,0,1,-1]
-        
-        var idx = 0
-        var cnt = 1
-        graph[y][x] = 0
-        
-        
-        
+
+        for i in 0..<4{
+            if (0..<size).contains(y + dy[i]) && (0..<size).contains(x + dx[i]) && map[y + dy[i]][x + dx[i]] == 1{
+                dfs(y + dy[i], x + dx[i])
+                cnt += 1
+            }
+        }
+        return cnt
     }
+
 }
 
-// bfs로 문제 해결
+// dfs로 문제 해결
+// 나중에 다시 풀기
