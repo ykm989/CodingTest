f52d8271dd27eb191ca4664c6cec2847c0a1d8fb
ykm989 <ykm989@naver.com>
2023-05-06 18:08:30 +0900

    1 solved

--- LeetCode.playground/Sources/1.swift
+++ LeetCode.playground/Sources/1.swift
@@ -20,3 +20,25 @@ class Solution {
         return result.sorted()
     }
 }
+
+/*
+ 다시 풀어봤다. two 포인터를 사용해볼까 하는대, 문제가 정렬된 array를 쓰지 않아서, 오답이다.
+ 그래서 다시 위와같은 array를 응용해볼까 했는데, 다른 사람들의 풀이를 보니 dictioanry를 사용한 사람이 있어서 그것을 사용해봤다.
+ 
+ 확실히 array를 사용하기 보다는, dictioanry의 값을 보는 읽는 것이 더 빠르다. 문제도 hashmap인거 보니 이게 맞다.
+ 
+ class Solution {
+     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+         var dic = [Int:Int]()
+
+         for (index, value) in nums.enumerated(){
+             if let two = dic[target - value]{
+                 return [two, index]
+             }
+             dic.updateValue(index, forKey: value)
+         }
+
+         return []
+     }
+ }
+ */
